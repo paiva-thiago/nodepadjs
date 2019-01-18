224cd0405e604f50674398a2635dfb4f59eb19e5
Thiago Paiva <paiva.thiago@gmail.com>
2019-01-18 17:28:29 -0200

    Mais de um texto a gravar - com correções a fazer!

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,20 +1,35 @@
 var page = record.corrente();
+var list = record.todos();
 new Vue({
     el: '#nodepad',
     data: {
         titulo: page.titulo,
         texto: page.texto,
+        gravados:list,
         displayPreview:true
     },
     computed: {
         compiledMarkdown: function () {
             var md       = window.markdownit();
             var rendered = md.render(this.texto); 
-            record.salvar(record.corrente().id,this.titulo,this.texto);
+            record.salvar(page.id,this.titulo,this.texto);
             return rendered;
         }
     },
     methods: {
+        selectText:function(pId){
+            var item=record.porId(pId);
+            this.titulo=item.titulo;
+            this.texto=item.texto;
+        },
+        novoTexto:function(){
+            console.log(record.todos());
+            var n = record.novoTexto();
+            this.gravados=record.todos();
+            this.titulo=n.titulo;
+            this.texto=n.texto;
+            console.log(record.todos());
+        },
         togglePreview:function () {
             this.displayPreview=!this.displayPreview;
         },
--- public/javascripts/service.js
+++ public/javascripts/service.js
@@ -1,3 +1,6 @@
+const PARAM         = 'data';
+const CURR_ID       = 'currentId';
+
 const text  = function(pId,pTitulo,pTexto){
     return {
         id:pId,
@@ -6,19 +9,12 @@ const text  = function(pId,pTitulo,pTexto){
     };
 } 
 
-const novo = function(pId){
-    return text(pId,'Novo Texto','# olá mundo! \n\n Comece a escrever aqui! \n\n -- \n\n Nodepad 2019');
-}
-
-const PARAM         = 'data';
-const CURR_ID       = 'currentId';
-
 
 
 const init = function(){
     var data = [];
     var id   = new Date().getTime();
-    var item = novo(id);
+    var item = text(id,'Novo Texto','# olá mundo! \n\n Comece a escrever aqui! \n\n -- \n\n Nodepad 2019');
     data.push(item);
     localStorage.setItem(PARAM,JSON.stringify(data));
     localStorage.setItem(CURR_ID,id);
@@ -37,22 +33,34 @@ const record = {
         }
         var data = this.todos();
         var item = data.filter(function(i){ return i.id==pId});
-        if(item==undefined){
-            item=novo(pId);
-            this.salvar(item.id,item.titulo,item.texto);
+        if((item==undefined)||(item[0]==undefined)){
+            console.error('Texto não encontrado!');
+            return undefined;
         }else{
             item=item[0];
         }
+        localStorage.setItem(CURR_ID,pId);
         return item;
 
     },
     corrente:function(){
-        return this.porId(localStorage.getItem(CURR_ID));
+        var id = localStorage.getItem(CURR_ID);
+        var item = this.porId(id);
+        if(item==undefined){
+            return  text(id,'Novo Texto','# olá mundo! \n\n Comece a escrever aqui! \n\n -- \n\n Nodepad 2019');
+        }
+        return item;
     },
+    novoTexto:function(){ 
+        var novoId=new Date().getTime();        
+        var item =  text(novoId,'Novo Texto','# olá mundo! \n\n Comece a escrever aqui! \n\n -- \n\n Nodepad 2019');        
+        localStorage.setItem(CURR_ID,novoId);
+        return item;
+     },
     salvar : function(pId,pTitulo,pTexto){
         var arrTodos  = this.todos();
         var old       = this.porId(pId);
-        var arrNovo   = arrTodos.filter(function(i){ console.log(i.id+' != '+pId+' = '+(i.id!=pId)); return i.id!=pId});
+        var arrNovo   = arrTodos.filter(function(i){ return i.id!=pId});
         var neo = text(pId,pTitulo,pTexto);
         arrNovo.push(neo);        
         localStorage.setItem(PARAM,JSON.stringify(arrNovo));
